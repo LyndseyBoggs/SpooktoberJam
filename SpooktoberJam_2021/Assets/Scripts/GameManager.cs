cd5aecc724094ffdb41f735e07e9cb78a9a61829
Alec <Quaintman2000@gmail.com>
2021-10-29 22:33:39 -0700

    Started on gamemanager

---
+++ SpooktoberJam_2021/Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Reference to the player.
+    // Reference to the cats.
+    // Score.
+    [SerializeField] float score;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
